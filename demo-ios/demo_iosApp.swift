cea1df0363e71b3fcc3a2e336eac188f587b68ba
Phann Phearoen <phannphearin2@gmail.com>
2024-07-07 08:40:22 +0700

    Initial Commit

---
+++ demo-ios/demo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_iosApp.swift
+//  demo-ios
+//
+//  Created by Phearoen Phann on 7/7/24.
+//
+
+import SwiftUI
+
+@main
+struct demo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
